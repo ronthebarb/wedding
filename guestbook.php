da70638d02c34234461bf77662e0b8f7f77d1680
Sam Ronan <41114967+ronthebarb@users.noreply.github.com>
2021-02-26 11:49:01 -0600

    Initial commit

---
+++ guestbook.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html Public "-//W3C//DTD XHTML 1.0 Strict//EN"
+        "http://www.w3.org/TR/xhtml/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="content-type" content="text/html; charset=iso-8859-1"/>
+    <title></title>
+    <link rel="stylesheet" href="css/guestbook.css">
+</head>
+<header>
+    <div class="container">
+        <nav>
+            <ul>
+                <li><a href="guestbook.php" id="guestbook">Sign Our Book</a></li>
+                <li><a href="engagement.html" id="where">Where</a></li>
+                <li><a href="home.html" id="home">Home</a></li>
+            </ul>
+        </nav>
+    </div>
+
+</header>
+<body>
+<?php
+
+?>
+</body>
+</html>
+
+
---
+++ script/slideshow.js
@@ -0,0 +1,20 @@
+vari
+
+/* var slideIndex = 0;
+showSlide();
+
+function showSlide() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    for (i=0, i < slides.length; i++;){
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length){
+        slideIndex = 1;
+    }
+    slides[slideIndex-1].style.display = "block";
+    setTimeout(showSlide, 2000);
+}
+
+ */
